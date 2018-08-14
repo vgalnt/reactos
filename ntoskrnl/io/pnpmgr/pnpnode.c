6a66be13649ea71081d34a0ef332b97bed326212
Vadim Galyant <vgal@rambler.ru>
2018-08-14 15:40:14 +0900

    [NTOS:PNP] Add ntoskrnl\io\pnpmgr\pnpnode.c file.

---
+++ ntoskrnl/io/pnpmgr/pnpnode.c
@@ -0,0 +1,24 @@
+/*
+ * PROJECT:         ReactOS Kernel
+ * COPYRIGHT:       GPL - See COPYING in the top level directory
+ * FILE:            ntoskrnl/io/pnpmgr/pnpnode.c
+ * PURPOSE:         Device node handle code
+ * PROGRAMMERS:     
+ */
+
+/* INCLUDES ******************************************************************/
+
+#include <ntoskrnl.h>
+#include "../pnpio.h"
+
+//#define NDEBUG
+#include <debug.h>
+
+/* GLOBALS *******************************************************************/
+
+/* DATA **********************************************************************/
+
+/* FUNCTIONS *****************************************************************/
+
+
+/* EOF */
