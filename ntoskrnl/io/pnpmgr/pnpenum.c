940e2c94c18fe2e05206c5ad973578650220a9c0
Vadim Galyant <vgal@rambler.ru>
2018-08-15 01:14:23 +0900

    [NTOS:PNP] Add ntoskrnl\io\pnpmgr\pnpenum.c file.

---
+++ ntoskrnl/io/pnpmgr/pnpenum.c
@@ -0,0 +1,24 @@
+/*
+ * PROJECT:         ReactOS Kernel
+ * COPYRIGHT:       GPL - See COPYING in the top level directory
+ * FILE:            ntoskrnl/io/pnpmgr/pnpenum.c
+ * PURPOSE:         Device enumeration functions
+ * PROGRAMMERS:     
+ */
+
+/* INCLUDES ******************************************************************/
+
+#include <ntoskrnl.h>
+#include "../pnpio.h"
+
+//#define NDEBUG
+#include <debug.h>
+
+/* GLOBALS *******************************************************************/
+
+/* DATA **********************************************************************/
+
+/* FUNCTIONS *****************************************************************/
+
+
+/* EOF */
