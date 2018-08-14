25974d55ca0fa1ca451190dee85bfd79417d88b2
Vadim Galyant <vgal@rambler.ru>
2018-08-15 00:38:32 +0900

    [NTOS:PNP] Add PipSetDevNodeState().

--- ntoskrnl/io/pnpmgr/pnpinit.c
+++ ntoskrnl/io/pnpmgr/pnpinit.c
@@ -40,6 +40,8 @@ ARBITER_INSTANCE IopRootPortArbiter;
 PDEVICE_NODE IopRootDeviceNode = NULL;
 LONG IopNumberDeviceNodes = 0;
 
+KSPIN_LOCK IopPnPSpinLock;
+
 /* FUNCTIONS ******************************************************************/
 
 VOID
@@ -612,6 +614,7 @@ IopInitializePlugPlayServices(
     }
 
     /* Initialize locks and such */
+    KeInitializeSpinLock(&IopPnPSpinLock);
     KeInitializeSpinLock(&IopDeviceTreeLock);
     KeInitializeSpinLock(&IopDeviceActionLock);
     InitializeListHead(&IopDeviceActionRequestList);
