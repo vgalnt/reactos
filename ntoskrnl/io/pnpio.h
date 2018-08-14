d7c8c78dfa91cf8fed170a9b2521ad20763a7be9
Vadim Galyant <vgal@rambler.ru>
2018-08-14 19:43:32 +0900

    [NTOS:PNP] Add IopDeviceObjectFromDeviceInstance().

--- ntoskrnl/io/pnpio.h
+++ ntoskrnl/io/pnpio.h
@@ -1,6 +1,12 @@
 #ifndef _PNPIO_H
 #define _PNPIO_H
 
+typedef struct _PNP_DEVICE_INSTANCE_CONTEXT
+{
+    PDEVICE_OBJECT DeviceObject;
+    PUNICODE_STRING InstancePath;
+} PNP_DEVICE_INSTANCE_CONTEXT, *PPNP_DEVICE_INSTANCE_CONTEXT;
+
 //
 // debug.c
 //
@@ -51,6 +57,15 @@ IopWriteResourceList(
     _In_ PUNICODE_STRING ValueName,
     _In_ PCM_RESOURCE_LIST CmResource,
     _In_ ULONG ListSize
+);
+
+//
+// pnputil.c
+//
+PDEVICE_OBJECT
+NTAPI
+IopDeviceObjectFromDeviceInstance(
+    _In_ PUNICODE_STRING DeviceInstance
 );
 
 #endif  /* _PNPIO_H */
