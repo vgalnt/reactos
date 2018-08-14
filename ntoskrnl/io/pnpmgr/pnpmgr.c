41bbfbbd38ff46468cd249576d11736216e448a4
Vadim Galyant <vgal@rambler.ru>
2018-08-14 16:32:16 +0900

    [NTOS:PNP] Move IopGetDeviceNode() and PipAllocateDeviceNode() from pmpmgr.c to pnpnode.c.

--- ntoskrnl/io/pnpmgr/pnpmgr.c
+++ ntoskrnl/io/pnpmgr/pnpmgr.c
@@ -62,13 +62,6 @@ IopPrepareDeviceForRemoval(PDEVICE_OBJECT DeviceObject, BOOLEAN Force);
 PDEVICE_OBJECT
 IopGetDeviceObjectFromDeviceInstance(PUNICODE_STRING DeviceInstance);
 
-PDEVICE_NODE
-FASTCALL
-IopGetDeviceNode(PDEVICE_OBJECT DeviceObject)
-{
-   return ((PEXTENDED_DEVOBJ_EXTENSION)DeviceObject->DeviceObjectExtension)->DeviceNode;
-}
-
 VOID
 IopFixupDeviceId(PWCHAR String)
 {
@@ -3750,49 +3743,6 @@ PpInitSystem(VOID)
         KeBugCheck(UNEXPECTED_INITIALIZATION_CALL);
         return FALSE;
     }
-}
-
-LONG IopNumberDeviceNodes;
-
-PDEVICE_NODE
-NTAPI
-PipAllocateDeviceNode(IN PDEVICE_OBJECT PhysicalDeviceObject)
-{
-    PDEVICE_NODE DeviceNode;
-    PAGED_CODE();
-
-    /* Allocate it */
-    DeviceNode = ExAllocatePoolWithTag(NonPagedPool, sizeof(DEVICE_NODE), TAG_IO_DEVNODE);
-    if (!DeviceNode) return DeviceNode;
-
-    /* Statistics */
-    InterlockedIncrement(&IopNumberDeviceNodes);
-
-    /* Set it up */
-    RtlZeroMemory(DeviceNode, sizeof(DEVICE_NODE));
-    DeviceNode->InterfaceType = InterfaceTypeUndefined;
-    DeviceNode->BusNumber = -1;
-    DeviceNode->ChildInterfaceType = InterfaceTypeUndefined;
-    DeviceNode->ChildBusNumber = -1;
-    DeviceNode->ChildBusTypeIndex = -1;
-//    KeInitializeEvent(&DeviceNode->EnumerationMutex, SynchronizationEvent, TRUE);
-    InitializeListHead(&DeviceNode->DeviceArbiterList);
-    InitializeListHead(&DeviceNode->DeviceTranslatorList);
-    InitializeListHead(&DeviceNode->TargetDeviceNotify);
-    InitializeListHead(&DeviceNode->DockInfo.ListEntry);
-    InitializeListHead(&DeviceNode->PendedSetInterfaceState);
-
-    /* Check if there is a PDO */
-    if (PhysicalDeviceObject)
-    {
-        /* Link it and remove the init flag */
-        DeviceNode->PhysicalDeviceObject = PhysicalDeviceObject;
-        ((PEXTENDED_DEVOBJ_EXTENSION)PhysicalDeviceObject->DeviceObjectExtension)->DeviceNode = DeviceNode;
-        PhysicalDeviceObject->Flags &= ~DO_DEVICE_INITIALIZING;
-    }
-
-    /* Return the node */
-    return DeviceNode;
 }
 
 /* PUBLIC FUNCTIONS **********************************************************/
