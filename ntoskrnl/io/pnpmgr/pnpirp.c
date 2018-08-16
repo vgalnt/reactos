f7b6bec1db9ca16c230cbbbf73498d7bfe0d4a5f
Vadim Galyant <vgal@rambler.ru>
2018-08-16 16:29:37 +0900

    [NTOS:PNP] Add ntoskrnl\io\pnpmgr\pnpirp.c file.

---
+++ ntoskrnl/io/pnpmgr/pnpirp.c
@@ -0,0 +1,22 @@
+/*
+ * PROJECT:         ReactOS Kernel
+ * LICENSE:         GPL - See COPYING in the top level directory
+ * FILE:            ntoskrnl/io/pnpmgr/pnpirp.c
+ * PURPOSE:         Code for IRP_MJ_PNP requests
+ * PROGRAMMERS:     
+ */
+
+/* INCLUDES *******************************************************************/
+
+#include <ntoskrnl.h>
+#include "../pnpio.h"
+
+//#define NDEBUG
+#include <debug.h>
+
+/* GLOBALS ********************************************************************/
+
+/* FUNCTIONS ******************************************************************/
+
+
+/* EOF */
