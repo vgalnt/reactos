d0fc544155b831e4bea285ae1a689016cd6caf70
Vadim Galyant <vgal@rambler.ru>
2018-08-19 15:15:50 +0900

    [HALX86] Correcting HalpAddDevice (fof legacy hals).

--- hal/halx86/include/legacy.h
+++ hal/halx86/include/legacy.h
@@ -16,6 +16,19 @@ typedef struct _HAL_PCI_IRQ_ROUTING_INFO
     ULONG Parameters;
 } HAL_PCI_IRQ_ROUTING_INFO, *PHAL_PCI_IRQ_ROUTING_INFO;
 
+PBUS_HANDLER
+FASTCALL
+HaliReferenceHandlerForBus(
+    IN INTERFACE_TYPE InterfaceType,
+    IN ULONG BusNumber
+);
+
+VOID
+FASTCALL
+HaliDereferenceBusHandler(
+    IN PBUS_HANDLER Handler
+);
+
 extern PCI_INT_ROUTE_INTERFACE PciIrqRoutingInterface;
 extern HAL_PCI_IRQ_ROUTING_INFO HalpPciIrqRoutingInfo;
 extern ULONG HalpIrqMiniportInitialized;
