8cce8f2a5a53b512d10d59ddf1abe4389907eec0
Vadim Galyant <vgal@rambler.ru>
2018-08-20 19:20:19 +0900

    [NTOS:PNP] Add ntoskrnl\io\pnpmgr\pnpmap.c file.

---
+++ ntoskrnl/io/pnpmgr/pnpmap.c
@@ -0,0 +1,24 @@
+/*
+ * PROJECT:         ReactOS Kernel
+ * COPYRIGHT:       GPL - See COPYING in the top level directory
+ * FILE:            ntoskrnl/io/pnpmgr/pnpreport.c
+ * PURPOSE:         PNP Mapper Functions
+ * PROGRAMMERS:     
+ */
+
+/* INCLUDES ******************************************************************/
+
+#include <ntoskrnl.h>
+#include "../pnpio.h"
+
+//#define NDEBUG
+#include <debug.h>
+
+/* GLOBALS *******************************************************************/
+
+/* TYPES *******************************************************************/
+
+/* PRIVATE FUNCTIONS *********************************************************/
+
+
+/* EOF */
