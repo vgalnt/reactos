648d038dd6189581ab74ff6006fabbd2d3d06c5a
Vadim Galyant <vgal@rambler.ru>
2018-08-29 11:33:06 +0900

    [SETUPAPI] Blocking code with old device node flag values.

--- dll/win32/setupapi/devinst.c
+++ dll/win32/setupapi/devinst.c
@@ -5419,7 +5419,8 @@ SetupDiInstallDevice(
             SetLastError(ERROR_GEN_FAILURE);
             goto cleanup;
         }
-        ConfigFlags |= DNF_DISABLED;
+        ASSERT(FALSE);//may be DNF_HAS_PROBLEM?
+        //ConfigFlags |= DNF_DISABLED;
         Result = SetupDiSetDeviceRegistryPropertyW(
             DeviceInfoSet,
             DeviceInfoData,
