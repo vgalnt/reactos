38b3ed9376246d0d7d402de52976910f7617c662
Vadim Galyant <vgal@rambler.ru>
2018-12-26 17:09:01 +0900

    [NTOSKRNL] After adding setupreg.hiv, correcting PipCallDriverAddDevice() and add "UpperFilters" strings to hivesys.inf.

--- ntoskrnl/io/pnpmgr/pnpinit.c
+++ ntoskrnl/io/pnpmgr/pnpinit.c
@@ -244,21 +244,6 @@ Quickie:
     return i;
 }
 
-static
-NTSTATUS
-NTAPI
-SetClassGuidValueKey(IN HANDLE ClassGuidHandle,
-                     IN PUNICODE_STRING KeyName,
-                     IN PUNICODE_STRING Key)
-{
-    return ZwSetValueKey(ClassGuidHandle,
-                         KeyName,
-                         0,
-                         REG_SZ,
-                         Key->Buffer,
-                         Key->Length + sizeof(UNICODE_NULL));
-}
-
 NTSTATUS
 NTAPI
 PipCallDriverAddDevice(IN PDEVICE_NODE DeviceNode,
@@ -327,13 +312,9 @@ PipCallDriverAddDevice(IN PDEVICE_NODE DeviceNode,
     if (DevType > 0)
     {
         UNICODE_STRING KeyName = RTL_CONSTANT_STRING(L"ClassGUID");
-        UNICODE_STRING Key;
-        ULONG Disposition = 0;
-        HANDLE ClassHandle=NULL;
-        HANDLE ClassGuidHandle=NULL;
 
-        DPRINT1("PipCallDriverAddDevice: InstancePath -'%wZ'\n", &DeviceNode->InstancePath);
-        DPRINT1("PipCallDriverAddDevice: ServiceName -'%wZ'\n", &DeviceNode->ServiceName);
+        DPRINT("PipCallDriverAddDevice: InstancePath -'%wZ'\n", &DeviceNode->InstancePath);
+        DPRINT("PipCallDriverAddDevice: ServiceName -'%wZ'\n", &DeviceNode->ServiceName);
 
         /* Create subkey ClassGUID for Device Instance key */
         if (DevType == 1 || DevType == 3)
@@ -358,175 +339,16 @@ PipCallDriverAddDevice(IN PDEVICE_NODE DeviceNode,
             goto Exit;
         }
 
-        /* Open the key */
+        /* Open the ..\Control\Class key */
         Status = IopOpenRegistryKeyEx(&ControlKey,
                                       NULL,
                                       &ControlClass,
                                       KEY_READ);
-
         if (!NT_SUCCESS(Status))
         {
             /* No class key */
             DPRINT1("PipCallDriverAddDevice: No key for '%wZ'\n", &ControlClass);
-
-            /* Create class key */
-            Status = IopCreateRegistryKeyEx(&ClassHandle,
-                                            NULL,
-                                            &ControlClass,
-                                            KEY_ALL_ACCESS,
-                                            REG_OPTION_NON_VOLATILE,
-                                            &Disposition);
-            if (!NT_SUCCESS(Status))
-            {
-                DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                goto Exit;
-            }
-
-            if (DevType == 1 || DevType == 3)
-            {
-                RtlInitUnicodeString(&ClassGuid, L"\\Registry\\Machine\\System\\CurrentControlSet\\Control\\Class\\{4D36E96B-E325-11CE-BFC1-08002BE10318}");
-            }
-            else if (DevType == 2 || DevType == 4)
-            {
-                RtlInitUnicodeString(&ClassGuid, L"\\Registry\\Machine\\System\\CurrentControlSet\\Control\\Class\\{4D36E96F-E325-11CE-BFC1-08002BE10318}");
-            }
-
-            Status = IopCreateRegistryKeyEx(&ClassGuidHandle,
-                                            NULL,
-                                            &ClassGuid,
-                                            KEY_ALL_ACCESS,
-                                            REG_OPTION_NON_VOLATILE,
-                                            &Disposition);
-            if (!NT_SUCCESS(Status))
-            {
-                DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                goto Exit;
-            }
-
-            if (DevType == 1 || DevType == 3)
-            {
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96B-E325-11CE-BFC1-08002BE10318}",,0x00000000,"Keyboard"
-                RtlInitUnicodeString(&KeyName, L"");
-                RtlInitUnicodeString(&Key, L"Keyboard");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96B-E325-11CE-BFC1-08002BE10318}","Class",0x00000000,"Keyboard"
-                RtlInitUnicodeString(&KeyName, L"Class");
-                RtlInitUnicodeString(&Key, L"Keyboard");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96B-E325-11CE-BFC1-08002BE10318}","Icon",0x00000000,"-3"
-                RtlInitUnicodeString(&KeyName, L"Icon");
-                RtlInitUnicodeString(&Key, L"-3");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96B-E325-11CE-BFC1-08002BE10318}","Installer32",0x00000000,"SysSetup.Dll,KeyboardClassInstaller"
-                RtlInitUnicodeString(&KeyName, L"Installer32");
-                RtlInitUnicodeString(&Key, L"SysSetup.Dll,KeyboardClassInstaller");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96B-E325-11CE-BFC1-08002BE10318}","NoInstallClass",0x00000000,"1"
-                RtlInitUnicodeString(&KeyName, L"NoInstallClass");
-                RtlInitUnicodeString(&Key, L"1");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96B-E325-11CE-BFC1-08002BE10318}","UpperFilters",0x00010000,"kbdclass"
-                RtlInitUnicodeString(&KeyName, L"UpperFilters");
-                RtlInitUnicodeString(&Key, L"kbdclass");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-            }
-            else if (DevType == 2 || DevType == 4)
-            {
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96F-E325-11CE-BFC1-08002BE10318}",,0x00000000,"Mouse"
-                RtlInitUnicodeString(&KeyName, L"");
-                RtlInitUnicodeString(&Key, L"Mouse");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96F-E325-11CE-BFC1-08002BE10318}","Class",0x00000000,"Mouse"
-                RtlInitUnicodeString(&KeyName, L"Class");
-                RtlInitUnicodeString(&Key, L"Mouse");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96F-E325-11CE-BFC1-08002BE10318}","Icon",0x00000000,"-2"
-                RtlInitUnicodeString(&KeyName, L"Icon");
-                RtlInitUnicodeString(&Key, L"-2");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96F-E325-11CE-BFC1-08002BE10318}","Installer32",0x00000000,"SysSetup.Dll,MouseClassInstaller"
-                RtlInitUnicodeString(&KeyName, L"Installer32");
-                RtlInitUnicodeString(&Key, L"SysSetup.Dll,MouseClassInstaller");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96F-E325-11CE-BFC1-08002BE10318}","NoInstallClass",0x00000000,"1"
-                RtlInitUnicodeString(&KeyName, L"NoInstallClass");
-                RtlInitUnicodeString(&Key, L"1");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-
-                //HKLM,"SYSTEM\CurrentControlSet\Control\Class\{4D36E96F-E325-11CE-BFC1-08002BE10318}","UpperFilters",0x00010000,"mouclass"
-                RtlInitUnicodeString(&KeyName, L"UpperFilters");
-                RtlInitUnicodeString(&Key, L"mouclass");
-                Status = SetClassGuidValueKey(ClassGuidHandle, &KeyName, &Key);
-                if (!NT_SUCCESS(Status))
-                {
-                    DPRINT("PipCallDriverAddDevice: Status -'%X'\n", Status);
-                    goto Exit;
-                }
-            }
+            ASSERT(FALSE);
         }
     }
 
