d79f482f34773e366564b4366823c11906560e10
Vadim Galyant <vgal@rambler.ru>
2019-06-03 20:22:27 +0900

    [NTOS:PNP] Add IopAllocateRelationList().

--- ntoskrnl/io/pnpmgr/pnpdel.c
+++ ntoskrnl/io/pnpmgr/pnpdel.c
@@ -9,9 +9,38 @@
 
 /* GLOBALS *******************************************************************/
 
+extern ULONG IopMaxDeviceNodeLevel; 
+
 /* DATA **********************************************************************/
 
 /* FUNCTIONS *****************************************************************/
+
+PRELATION_LIST
+NTAPI
+IopAllocateRelationList(
+    _In_ PIP_TYPE_REMOVAL_DEVICE DeleteType)
+{
+    PRELATION_LIST RelationsList;
+    ULONG Size;
+
+    PAGED_CODE();
+    DPRINT("IopAllocateRelationList: DeleteType - %X, IopMaxDeviceNodeLevel - %X\n", DeleteType, IopMaxDeviceNodeLevel);
+
+    Size = sizeof(RELATION_LIST) + IopMaxDeviceNodeLevel * sizeof(PRELATION_LIST_ENTRY);
+
+    RelationsList = PiAllocateCriticalMemory(DeleteType, PagedPool, Size, 'rcpP');
+    if (!RelationsList)
+    {
+        DPRINT1("IopAllocateRelationList: fail PiAllocateCriticalMemory()\n");
+        ASSERT(FALSE);
+        return RelationsList;
+    }
+
+    RtlZeroMemory(RelationsList, Size);
+    RelationsList->MaxLevel = IopMaxDeviceNodeLevel;
+
+    return RelationsList;
+}
 
 
 /* EOF */
