7ca7861a5146a9366ed1f33117c26e8dff1853f6
Timo Kreuzer <timo.kreuzer@reactos.org>
2020-01-26 18:59:05 +0100

    [KDBG] Fix x64 KDBG
    # Conflicts:
    #	ntoskrnl/ex/handle.c

--- ntoskrnl/ex/handle.c
+++ ntoskrnl/ex/handle.c
@@ -22,7 +22,13 @@ EX_PUSH_LOCK HandleTableListLock;
 
 /* PRIVATE FUNCTIONS *********************************************************/
 
-INIT_FUNCTION
+#ifdef _WIN64
+#define strtoulptr strtoull
+#else
+#define strtoulptr strtoul
+#endif
+
+CODE_SEG("INIT")
 VOID
 NTAPI
 ExpInitializeHandleTables(VOID)
@@ -1360,7 +1366,7 @@ BOOLEAN ExpKdbgExtHandle(ULONG Argc, PCHAR Argv[])
         }
         else
         {
-            ProcessId = (HANDLE)strtoul(Argv[1], &endptr, 10);
+            ProcessId = (HANDLE)strtoulptr(Argv[1], &endptr, 10);
             if (*endptr != '\0')
             {
                 KdbpPrint("Invalid parameter: %s\n", Argv[1]);
