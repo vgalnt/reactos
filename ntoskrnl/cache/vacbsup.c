ecf919c6e764063380ae5b6dcce238f2238437df
Vadim Galyant <vgal@rambler.ru>
2020-03-05 06:45:03 +0900

    [NTOS:NEWCC] Add new "ntoskrnl/cache/vacbsup.c".

---
+++ ntoskrnl/cache/vacbsup.c
@@ -0,0 +1,24 @@
+/*
+ * COPYRIGHT:       See COPYING in the top level directory
+ * PROJECT:         ReactOS Kernel
+ * FILE:            ntoskrnl/cache/vacbsup.c
+ * PURPOSE:         Virtual Address Command Block (VACB) support
+ * PROGRAMMERS:     
+ */
+
+/* INCLUDES *******************************************************************/
+
+#include <ntoskrnl.h>
+#include "newcc.h"
+#include "section/newmm.h"
+//#define NDEBUG
+#include <debug.h>
+
+/* GLOBALS ********************************************************************/
+
+/* STRUCTURES *****************************************************************/
+
+/* PRIVATE FUNCTIONS **********************************************************/
+
+
+/* EOF */
