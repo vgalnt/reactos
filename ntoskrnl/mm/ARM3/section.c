0fbe348c277a0426cbe3c9ad1343844129fc8d8c
Vadim Galyant <vgal@rambler.ru>
2020-03-11 10:39:53 +0400

    [NTOS:MM] Add MiCheckProtoPtePageState().

--- ntoskrnl/mm/ARM3/section.c
+++ ntoskrnl/mm/ARM3/section.c
@@ -5533,6 +5533,63 @@ Exit:
     return TRUE;
 }
 
+BOOLEAN
+NTAPI
+MiCheckProtoPtePageState(PMMPTE SectionProto,
+                         KIRQL OldIrql,
+                         BOOLEAN * OutIsLock)
+{
+    PMMPTE ProtoPte;
+    MMPTE TempPte;
+    PMMPFN Pfn;
+
+    DPRINT("MiCheckProtoPtePageState: SectionProto %p, OldIrql %X\n", SectionProto, OldIrql);
+
+    *OutIsLock = FALSE;
+
+    ProtoPte = MiAddressToPte(SectionProto);
+
+    if (!ProtoPte->u.Hard.Valid)
+    {
+        MiCheckPdeForPagedPool(SectionProto);
+    }
+
+    TempPte.u.Long = ProtoPte->u.Long;
+
+    if (TempPte.u.Hard.Valid)
+    {
+        Pfn = MI_PFN_ELEMENT(TempPte.u.Hard.PageFrameNumber);
+
+        if (Pfn->u2.ShareCount == 1)
+        {
+            return FALSE;
+        }
+
+        return TRUE;
+    }
+
+    if (!TempPte.u.Soft.Prototype &&
+        TempPte.u.Soft.Transition)
+    {
+        return FALSE;
+    }
+
+    Pfn = MI_PFN_ELEMENT(TempPte.u.Trans.PageFrameNumber);
+
+    if (Pfn->u3.e1.PageLocation < ActiveAndValid)
+    {
+        return FALSE;
+    }
+
+    if (OldIrql != MM_NOIRQL)
+    {
+        MiMakeSystemAddressValidPfn(SectionProto, OldIrql);
+        *OutIsLock = TRUE;
+    }
+
+    return TRUE;
+}
+
 /* SYSTEM CALLS ***************************************************************/
 
 NTSTATUS
