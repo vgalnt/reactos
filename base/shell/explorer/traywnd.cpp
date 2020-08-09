faaf87e3bef8bee93a4962c698ef661d0b850a8f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2020-08-10 07:15:21 +0900

    [EXPLORER] Improve CTrayWindow::OnCopyData (#3028)
    Check pCopyData->dwData value. Don't send message needlessly.
    CORE-7237

--- base/shell/explorer/traywnd.cpp
+++ base/shell/explorer/traywnd.cpp
@@ -23,6 +23,7 @@
 #include <commoncontrols.h>
 
 HRESULT TrayWindowCtxMenuCreator(ITrayWindow * TrayWnd, IN HWND hWndOwner, IContextMenu ** ppCtxMenu);
+LRESULT appbar_message(COPYDATASTRUCT* cds);
 
 #define WM_APP_TRAYDESTROY  (WM_APP + 0x100)
 
@@ -2196,7 +2197,6 @@ ChangePos:
         return m_ContextMenu->GetCommandString(idCmd, uType, pwReserved, pszName, cchMax);
     }
 
-
     /**********************************************************
      *    ##### message handling #####
      */
@@ -2340,9 +2340,16 @@ ChangePos:
 
     LRESULT OnCopyData(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
     {
-        if (m_TrayNotify)
-            ::SendMessageW(m_TrayNotify, uMsg, wParam, lParam);
-        return TRUE;
+        COPYDATASTRUCT *pCopyData = reinterpret_cast<COPYDATASTRUCT *>(lParam);
+        switch (pCopyData->dwData)
+        {
+            case TABDMC_APPBAR:
+                return appbar_message(pCopyData);
+            case TABDMC_NOTIFY:
+            case TABDMC_LOADINPROC:
+                return ::SendMessageW(m_TrayNotify, uMsg, wParam, lParam);
+        }
+        return FALSE;
     }
 
     LRESULT OnNcPaint(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
