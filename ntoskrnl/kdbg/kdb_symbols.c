0a98fd00920d748e8c916ff136fa13e895b82e3a
Serge Gautherie <reactos-git_serge_171003@gautherie.fr>
2020-09-09 03:46:56 +0200

    [NTOS:KDBG] Add some missing OBJ_KERNEL_HANDLE
    CORE-10207

--- ntoskrnl/kdbg/kdb_symbols.c
+++ ntoskrnl/kdbg/kdb_symbols.c
@@ -384,7 +384,7 @@ KdbpSymLoadModuleSymbols(
     /*  Open the file  */
     InitializeObjectAttributes(&ObjectAttributes,
                                FileName,
-                               0,
+                               OBJ_CASE_INSENSITIVE | OBJ_KERNEL_HANDLE,
                                NULL,
                                NULL);
 
