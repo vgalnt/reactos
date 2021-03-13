9ae240fa225eb57f51457ac59c6286a3ea8ec0fa
George Bișoc <george.bisoc@reactos.org>
2021-03-13 20:45:15 +0100

    [NTOS:SE] Specify the code section of SepCreateSystemProcessToken as INIT
    This function is used during the Security kernel module phase initialisation to set up the system process token which the phase initialisation procedure in itself is stored in the INIT section. With that being said, do the same for SepCreateSystemProcessToken too and add a header documentation as an addition.

--- ntoskrnl/se/token.c
+++ ntoskrnl/se/token.c
@@ -1139,6 +1139,15 @@ Quit:
     return Status;
 }
 
+/**
+ * @brief
+ * Creates the system process token.
+ *
+ * @return
+ * Returns the system process token if the operations have
+ * completed successfully.
+ */
+CODE_SEG("INIT")
 PTOKEN
 NTAPI
 SepCreateSystemProcessToken(VOID)
