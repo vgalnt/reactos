638a35c3fbe5c360c9bb7d6d5266b6dd94010120
George Bișoc <george.bisoc@reactos.org>
2021-12-26 20:58:28 +0100

    [NTOSKRNL] Regroup the pool allocation tags in one dedicated place
    We have a special file, tag.h, which serves as a place to store whatever kernel pool allocation tag yet we still have some tags sparse over the kernel code... So just re-group them in one unique place.
    
    # Conflicts:
    #	ntoskrnl/ex/profile.c
    #	ntoskrnl/io/pnpmgr/devaction.c

--- ntoskrnl/ke/bug.c
+++ ntoskrnl/ke/bug.c
@@ -32,7 +32,6 @@ ULONG_PTR KiBugCheckData[5];
 
 PKNMI_HANDLER_CALLBACK KiNmiCallbackListHead = NULL;
 KSPIN_LOCK KiNmiCallbackListLock;
-#define TAG_KNMI 'IMNK'
 
 /* Bugzilla Reporting */
 UNICODE_STRING KeRosProcessorName, KeRosBiosDate, KeRosBiosVersion;
