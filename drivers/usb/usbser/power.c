61ae5bb61ef5c93d88431983c6c6fd063f82a77f
Vadim Galyant <vgal@rambler.ru>
2022-02-23 21:07:36 +0400

    [USBSER] Add null files for splitting code.

---
+++ drivers/usb/usbser/power.c
@@ -0,0 +1,22 @@
+/*
+ * PROJECT:     Universal serial bus modem driver
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     USB modem driver power functions.
+ * COPYRIGHT:   Copyright 2022 Vadim Galyant <vgal@rambler.ru>
+ */
+
+/* INCLUDES ******************************************************************/
+
+#include "usbser.h"
+
+//#define NDEBUG
+#include <debug.h>
+
+/* DATA ***********************************************************************/
+
+/* GLOBALS ********************************************************************/
+
+/* FUNCTIONS *****************************************************************/
+
+
+/* EOF */
