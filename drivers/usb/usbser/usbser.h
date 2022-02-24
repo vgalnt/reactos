0666383888104a3d4a7a0d38f910b3cf1e44b9a8
Vadim Galyant <vgal@rambler.ru>
2022-02-24 23:03:36 +0400

    [USBSER] Add struct _USBSER_CDC_NOTIFICATION and struct _USBSER_CDC_LINE_CODING.

--- drivers/usb/usbser/usbser.h
+++ drivers/usb/usbser/usbser.h
@@ -17,6 +17,50 @@
 #define USBSER_MAX_SLOT 256
 #define USBSER_TAG 'CBSU'
 
+/* Universal Serial Bus Class Definitions for Communications Devices
+   Revision 1.2, November 3, 2010 (CDC120)
+*/
+/* Universal Serial Bus Communications Class Subclass Specification for PSTN Devices
+   Revision 1.2, February 9, 2007 (PSTN120)
+*/
+#include <pshpack1.h>
+typedef union _USBSER_SERIAL_STATE
+{
+    struct
+    {
+        USHORT RxCarrier:1;  // State of receiver carrier detection mechanism of device. This corresponds to V.24 signal 109 and RS-232 signal DCD. 
+        USHORT TxCarrier:1;  // State of transmission carrier. This signal corresponds to V.24 RS-232 signal DSR. 
+        USHORT Break:1;      // State of break detection mechanism of the device. 
+        USHORT RingSignal:1; // State of ring signal detection of the device. 
+        USHORT Framing:1;    // A framing error has occurred. 
+        USHORT Parity:1;     // A parity error has occurred. 
+        USHORT OverRun:1;    // Received data has been discarded due to overrun in the device
+        USHORT Reserved:9;
+    };
+    USHORT AsUSHORT;
+} USBSER_SERIAL_STATE, *PUSBSER_SERIAL_STATE;
+
+typedef struct _USBSER_CDC_NOTIFICATION
+{
+    UCHAR RequestType;
+    UCHAR NotificationType;
+    USHORT Value;
+    USHORT Index;
+    USHORT Length;
+    USBSER_SERIAL_STATE SerialState;
+} USBSER_CDC_NOTIFICATION, *PUSBSER_CDC_NOTIFICATION;
+
+typedef struct _USBSER_CDC_LINE_CODING
+{
+    ULONG BaudRate;   // Data terminal rate, in bits per second
+    UCHAR StopBits;
+    UCHAR ParityType;
+    UCHAR DataBits;   // Data bits (5, 6, 7, 8 or 16)
+} USBSER_CDC_LINE_CODING, *PUSBSER_CDC_LINE_CODING;
+#include <poppack.h>
+C_ASSERT(sizeof(USBSER_CDC_NOTIFICATION) == 0xA);
+C_ASSERT(sizeof(USBSER_CDC_LINE_CODING) == 0x7);
+
 typedef struct _USBSER_DEVICE_EXTENSION
 {
     PDEVICE_OBJECT PhysicalDevice;
