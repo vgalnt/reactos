2851dbcce5e19d7348bc0649c0f230e0628d26da
Vadim Galyant <vgal@rambler.ru>
2023-11-04 20:29:20 +0300

    [PCIX] Refactoring PciDispatchIrp().

--- drivers/bus/pcix/dispatch.c
+++ drivers/bus/pcix/dispatch.c
@@ -96,23 +96,27 @@ PciPassIrpFromFdoToPdo(IN PPCI_FDO_EXTENSION DeviceExtension,
 
 NTSTATUS
 NTAPI
-PciDispatchIrp(IN PDEVICE_OBJECT DeviceObject,
-               IN PIRP Irp)
+PciDispatchIrp(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _In_ PIRP Irp)
 {
+    PCI_DISPATCH_FUNCTION DispatchFunction;
+    PPCI_MJ_DISPATCH_TABLE IrpDispatchTable;
+    PPCI_MN_DISPATCH_TABLE TableArray;
+    PPCI_MN_DISPATCH_TABLE Table;
+    PCI_DISPATCH_STYLE DispatchStyle;
     PPCI_FDO_EXTENSION DeviceExtension;
     PIO_STACK_LOCATION IoStackLocation;
-    PPCI_MJ_DISPATCH_TABLE IrpDispatchTable;
+    USHORT MaxMinor;
     BOOLEAN PassToPdo;
     NTSTATUS Status;
-    PPCI_MN_DISPATCH_TABLE TableArray = NULL, Table;
-    USHORT MaxMinor;
-    PCI_DISPATCH_STYLE DispatchStyle = 0;
-    PCI_DISPATCH_FUNCTION DispatchFunction = NULL;
-    DPRINT1("PCI: Dispatch IRP\n");
+
+    DPRINT("PciDispatchIrp: %p, %p\n", DeviceObject, Irp);
 
     /* Get the extension and I/O stack location for this IRP */
-    DeviceExtension = (PPCI_FDO_EXTENSION)DeviceObject->DeviceExtension;
+    DeviceExtension = DeviceObject->DeviceExtension;
     IoStackLocation = IoGetCurrentIrpStackLocation(Irp);
+
     ASSERT((DeviceExtension->ExtensionType == PciPdoExtensionType) ||
            (DeviceExtension->ExtensionType == PciFdoExtensionType));
 
@@ -120,140 +124,131 @@ PciDispatchIrp(IN PDEVICE_OBJECT DeviceObject,
     if (DeviceExtension->DeviceState == PciDeleted)
     {
         /* Fail this IRP */
+        DPRINT1("PciDispatchIrp: STATUS_NO_SUCH_DEVICE\n");
         Status = STATUS_NO_SUCH_DEVICE;
         PassToPdo = FALSE;
+        goto Finish;
     }
-    else
+
+    /* Otherwise, get the dispatch table for the extension */
+    IrpDispatchTable = DeviceExtension->IrpDispatchTable;
+
+    /* And choose which function table to use */
+    switch (IoStackLocation->MajorFunction)
     {
-        /* Otherwise, get the dispatch table for the extension */
-        IrpDispatchTable = DeviceExtension->IrpDispatchTable;
+        case IRP_MJ_POWER:
 
-        /* And choose which function table to use */
-        switch (IoStackLocation->MajorFunction)
-        {
-            case IRP_MJ_POWER:
+            /* Power Manager IRPs */
+            TableArray = IrpDispatchTable->PowerIrpDispatchTable;
+            MaxMinor = IrpDispatchTable->PowerIrpMaximumMinorFunction;
+            break;
 
-                /* Power Manager IRPs */
-                TableArray = IrpDispatchTable->PowerIrpDispatchTable;
-                MaxMinor = IrpDispatchTable->PowerIrpMaximumMinorFunction;
-                break;
+        case IRP_MJ_PNP:
 
-            case IRP_MJ_PNP:
+            /* Plug-and-Play Manager IRPs */
+            TableArray = IrpDispatchTable->PnpIrpDispatchTable;
+            MaxMinor = IrpDispatchTable->PnpIrpMaximumMinorFunction;
+            break;
 
-                /* Plug-and-Play Manager IRPs */
-                TableArray = IrpDispatchTable->PnpIrpDispatchTable;
-                MaxMinor = IrpDispatchTable->PnpIrpMaximumMinorFunction;
-                break;
+        case IRP_MJ_SYSTEM_CONTROL:
 
-            case IRP_MJ_SYSTEM_CONTROL:
+            /* WMI IRPs */
+            DispatchFunction = IrpDispatchTable->SystemControlIrpDispatchFunction;
+            DispatchStyle = IrpDispatchTable->SystemControlIrpDispatchStyle;
+            MaxMinor = 0xFFFF;
+            break;
 
-                /* WMI IRPs */
-                DispatchFunction = IrpDispatchTable->SystemControlIrpDispatchFunction;
-                DispatchStyle = IrpDispatchTable->SystemControlIrpDispatchStyle;
-                MaxMinor = 0xFFFF;
-                break;
+        default:
 
-            default:
+            DPRINT1("PciDispatchIrp: Other IRPs, MajorFunction %X\n", IoStackLocation->MajorFunction);
+            DispatchFunction = IrpDispatchTable->OtherIrpDispatchFunction;
+            DispatchStyle = IrpDispatchTable->OtherIrpDispatchStyle;
+            MaxMinor = 0xFFFF;
+            break;
+    }
 
-                /* Unrecognized IRPs */
-                DispatchFunction = IrpDispatchTable->OtherIrpDispatchFunction;
-                DispatchStyle = IrpDispatchTable->OtherIrpDispatchStyle;
-                MaxMinor = 0xFFFF;
-                break;
-        }
+    /* Only deal with recognized IRPs */
+    if (MaxMinor != 0xFFFF)
+    {
+        /* Make sure the function is recognized */
+        if (IoStackLocation->MinorFunction > MaxMinor)
+            /* Pick the terminator, which should return unrecognized */
+            Table = &TableArray[MaxMinor + 1];
+        else
+            /* Pick the appropriate table for this function */
+            Table = &TableArray[IoStackLocation->MinorFunction];
 
-        /* Only deal with recognized IRPs */
-        if (MaxMinor != 0xFFFF)
-        {
-            /* Make sure the function is recognized */
-            if (IoStackLocation->MinorFunction > MaxMinor)
-            {
-                /* Pick the terminator, which should return unrecognized */
-                Table = &TableArray[MaxMinor + 1];
-            }
-            else
-            {
-                /* Pick the appropriate table for this function */
-                Table = &TableArray[IoStackLocation->MinorFunction];
-            }
+        /* From that table, get the function code and dispatch style */
+        DispatchStyle = Table->DispatchStyle;
+        DispatchFunction = Table->DispatchFunction;
+    }
 
-            /* From that table, get the function code and dispatch style */
-            DispatchStyle = Table->DispatchStyle;
-            DispatchFunction = Table->DispatchFunction;
-        }
+    /* Print out debugging information, and see if we should break */
+    if (PciDebugIrpDispatchDisplay(IoStackLocation, DeviceExtension, MaxMinor))
+    {
+        /* The developer/user wants us to break for this IRP, do it */
+        DbgBreakPoint();
+    }
 
-        /* Print out debugging information, and see if we should break */
-        if (PciDebugIrpDispatchDisplay(IoStackLocation,
-                                       DeviceExtension,
-                                       MaxMinor))
-        {
-            /* The developer/user wants us to break for this IRP, do it */
-            DbgBreakPoint();
-        }
+    /* Check if this IRP should be sent up the stack first */
+    if (DispatchStyle == IRP_UPWARD)
+        /* Do it now before handling it ourselves */
+        PciCallDownIrpStack(DeviceExtension, Irp);
 
-        /* Check if this IRP should be sent up the stack first */
-        if (DispatchStyle == IRP_UPWARD)
-        {
-            /* Do it now before handling it ourselves */
-            PciCallDownIrpStack(DeviceExtension, Irp);
-        }
+    /* Call the our driver's handler for this IRP and deal with the IRP */
+    Status = DispatchFunction(Irp, IoStackLocation, DeviceExtension);
 
-        /* Call the our driver's handler for this IRP and deal with the IRP */
-        Status = DispatchFunction(Irp, IoStackLocation, DeviceExtension);
-        switch (DispatchStyle)
-        {
-            /* Complete IRPs are completely immediately by our driver */
-            case IRP_COMPLETE:
-                PassToPdo = FALSE;
-                break;
+    switch (DispatchStyle)
+    {
+        /* Complete IRPs are completely immediately by our driver */
+        case IRP_COMPLETE:
+            PassToPdo = FALSE;
+            break;
 
-            /* Downward IRPs are send to the attached FDO */
-            case IRP_DOWNWARD:
-                PassToPdo = TRUE;
-                break;
+        /* Downward IRPs are send to the attached FDO */
+        case IRP_DOWNWARD:
+            PassToPdo = TRUE;
+            break;
 
-            /* Upward IRPs are completed immediately by our driver */
-            case IRP_UPWARD:
-                PassToPdo = FALSE;
-                break;
+        /* Upward IRPs are completed immediately by our driver */
+        case IRP_UPWARD:
+            PassToPdo = FALSE;
+            break;
 
-            /* Dispatch IRPs are immediately returned */
-            case IRP_DISPATCH:
-                return Status;
+        /* Dispatch IRPs are immediately returned */
+        case IRP_DISPATCH:
+            return Status;
 
-            /* There aren't any other dispatch styles! */
-            default:
-                ASSERT(FALSE);
-                return Status;
-        }
+        /* There aren't any other dispatch styles! */
+        default:
+            DPRINT1("PciDispatchIrp: unknown DispatchStyle %X\n", DispatchStyle);
+            ASSERT(FALSE);
+            return Status;
     }
 
     /* Pending IRPs are returned immediately */
-    if (Status == STATUS_PENDING) return Status;
+    if (Status == STATUS_PENDING)
+        return Status;
+
+Finish:
 
     /* Handled IRPs return their status in the status block */
-    if (Status != STATUS_NOT_SUPPORTED) Irp->IoStatus.Status = Status;
+    if (Status != STATUS_NOT_SUPPORTED)
+        Irp->IoStatus.Status = Status;
 
     /* Successful, or unhandled IRPs that are "DOWNWARD" are sent to the PDO */
-    if ((PassToPdo) && ((NT_SUCCESS(Status)) || (Status == STATUS_NOT_SUPPORTED)))
-    {
+    if (PassToPdo && (NT_SUCCESS(Status) || Status == STATUS_NOT_SUPPORTED))
         /* Let the PDO deal with it */
-        Status = PciPassIrpFromFdoToPdo(DeviceExtension, Irp);
-    }
-    else
-    {
-        /* Otherwise, the IRP is returned with its status */
-        Status = Irp->IoStatus.Status;
+        return PciPassIrpFromFdoToPdo(DeviceExtension, Irp);
 
-        /* Power IRPs need to notify the Power Manager that the next IRP can go */
-        if (IoStackLocation->MajorFunction == IRP_MJ_POWER) PoStartNextPowerIrp(Irp);
+    /* Power IRPs need to notify the Power Manager that the next IRP can go */
+    if (IoStackLocation->MajorFunction == IRP_MJ_POWER)
+        PoStartNextPowerIrp(Irp);
 
-        /* And now this IRP can be completed */
-        IoCompleteRequest(Irp, IO_NO_INCREMENT);
-    }
+    /* And now this IRP can be completed */
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
 
-    /* And the status returned back to the caller */
-    return Status;
+    return Irp->IoStatus.Status;
 }
 
 NTSTATUS
