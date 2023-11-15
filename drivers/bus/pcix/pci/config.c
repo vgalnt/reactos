03d9e92ea95b8a4b91505fdc3b454b2bff7e36ba
Vadim Galyant <vgal@rambler.ru>
2023-11-15 21:57:52 +0300

    [PCIX] Refactoring PciReadWriteConfigSpace().

--- drivers/bus/pcix/pci/config.c
+++ drivers/bus/pcix/pci/config.c
@@ -46,44 +46,47 @@ PciGetAdjustedInterruptLine(IN PPCI_PDO_EXTENSION PdoExtension)
 
 VOID
 NTAPI
-PciReadWriteConfigSpace(IN PPCI_FDO_EXTENSION DeviceExtension,
-                        IN PCI_SLOT_NUMBER Slot,
-                        IN PVOID Buffer,
-                        IN ULONG Offset,
-                        IN ULONG Length,
-                        IN BOOLEAN Read)
+PciReadWriteConfigSpace(
+    _In_ PPCI_FDO_EXTENSION ParentFdoExt,
+    _In_ PCI_SLOT_NUMBER Slot,
+    _In_ PVOID Buffer,
+    _In_ ULONG Offset,
+    _In_ ULONG Length,
+    _In_ BOOLEAN IsRead)
 {
     PPCI_BUS_INTERFACE_STANDARD PciInterface;
     PBUS_HANDLER BusHandler;
     PPCIBUSDATA BusData;
     PciReadWriteConfig HalFunction;
 
+    DPRINT("PciReadWriteConfigSpace: %X, %X, %X, %X\n", Slot.u.AsULONG, Offset, Length, IsRead);
+
     /* Only the root FDO can access configuration space */
-    ASSERT(PCI_IS_ROOT_FDO(DeviceExtension->BusRootFdoExtension));
+    ASSERT(PCI_IS_ROOT_FDO(ParentFdoExt->BusRootFdoExtension));
 
     /* Get the ACPI-compliant PCI interface */
-    PciInterface = DeviceExtension->BusRootFdoExtension->PciBusInterface;
-    if (PciInterface)
-    {
-        /* Currently this driver only supports the legacy HAL interface */
-        UNIMPLEMENTED_DBGBREAK();
-    }
-    else
+    PciInterface = ParentFdoExt->BusRootFdoExtension->PciBusInterface;
+    if (!PciInterface)
     {
         /* Make sure there's a registered HAL bus handler */
-        ASSERT(DeviceExtension->BusHandler);
+        ASSERT(ParentFdoExt->BusHandler);
 
         /* PCI Bus Number assignment is only valid on ACPI systems */
         ASSERT(!PciAssignBusNumbers);
 
         /* Grab the HAL PCI Bus Handler data */
-        BusHandler = (PBUS_HANDLER)DeviceExtension->BusHandler;
+        BusHandler = (PBUS_HANDLER)ParentFdoExt->BusHandler;
         BusData = (PPCIBUSDATA)BusHandler->BusData;
 
         /* Choose the appropriate read or write function, and call it */
-        HalFunction = Read ? BusData->ReadConfig : BusData->WriteConfig;
+        HalFunction = (IsRead ? BusData->ReadConfig : BusData->WriteConfig);
         HalFunction(BusHandler, Slot, Buffer, Offset, Length);
+
+        return;
     }
+
+    /* Currently this driver only supports the legacy HAL interface */
+    UNIMPLEMENTED_DBGBREAK();
 }
 
 VOID
