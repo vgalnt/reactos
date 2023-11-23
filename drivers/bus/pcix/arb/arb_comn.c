8925d5b27c58544eb662013a9380f73543c46f3e
Vadim Galyant <vgal@rambler.ru>
2023-11-23 11:26:51 +0300

    [PCIX] Add PciReferenceArbiter().

--- drivers/bus/pcix/arb/arb_comn.c
+++ drivers/bus/pcix/arb/arb_comn.c
@@ -27,6 +27,19 @@ PCHAR PciArbiterNames[] =
 
 VOID
 NTAPI
+PciReferenceArbiter(
+    _In_ PVOID Context)
+{
+    PPCI_ARBITER_INSTANCE Instance;
+
+    DPRINT("PciReferenceArbiter: %p\n", Context);
+
+    Instance = CONTAINING_RECORD(Context, PCI_ARBITER_INSTANCE, CommonInstance);
+    InterlockedIncrement(&Instance->CommonInstance.ReferenceCount);
+}
+
+VOID
+NTAPI
 PciArbiterDestructor(IN PPCI_ARBITER_INSTANCE Arbiter)
 {
     UNREFERENCED_PARAMETER(Arbiter);
