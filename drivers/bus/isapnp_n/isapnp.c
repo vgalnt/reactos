5c2069e5e8194d6f72cd6cde39d27bcf1b81f60f
Vadim Galyant <vgal@rambler.ru>
2023-12-03 23:36:34 +0300

    [ISAPNP_N] Add DriverEntry() and stubs.

--- drivers/bus/isapnp_n/isapnp.c
+++ drivers/bus/isapnp_n/isapnp.c
@@ -14,8 +14,102 @@
 
 /* GLOBALS *******************************************************************/
 
+PDRIVER_OBJECT PipDriverObject;
+UNICODE_STRING PipRegistryPath;
+
+ULONG BusNumberBuffer[0x40];
+RTL_BITMAP BusNumBMHeader;
+PRTL_BITMAP BusNumBM;
 
 /* FUNCTIONS ******************************************************************/
 
+NTSTATUS
+NTAPI
+PiDispatchPnp(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _In_ PIRP Irp)
+{
+    UNIMPLEMENTED_DBGBREAK();
+    return STATUS_NOT_IMPLEMENTED;
+}
 
+NTSTATUS
+NTAPI
+PiDispatchPower(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _In_ PIRP Irp)
+{
+    UNIMPLEMENTED_DBGBREAK();
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+PiDispatchDevCtl(
+    _In_ PDEVICE_OBJECT DeviceObject,
+    _In_ PIRP Irp)
+{
+    UNIMPLEMENTED_DBGBREAK();
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+VOID
+NTAPI
+PiUnload(
+    _In_ PDRIVER_OBJECT DriverObject)
+{
+    UNIMPLEMENTED_DBGBREAK();
+}
+
+NTSTATUS
+NTAPI
+PiAddDevice(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PDEVICE_OBJECT TargetDevice)
+{
+    UNIMPLEMENTED_DBGBREAK();
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+DriverEntry(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath)
+{
+    DPRINT("ISAPNP: DriverEntry(%p, '%wZ')\n", DriverObject, RegistryPath);
+
+    PipDriverObject = DriverObject;
+
+    DriverObject->MajorFunction[IRP_MJ_PNP] = PiDispatchPnp;
+    DriverObject->MajorFunction[IRP_MJ_POWER] = PiDispatchPower;
+    DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = PiDispatchDevCtl;
+    DriverObject->MajorFunction[IRP_MJ_SYSTEM_CONTROL] = PiDispatchDevCtl;
+
+    DriverObject->DriverUnload = PiUnload;
+    DriverObject->DriverExtension->AddDevice = PiAddDevice;
+
+    PipRegistryPath.Length = RegistryPath->Length;
+    PipRegistryPath.MaximumLength = RegistryPath->MaximumLength;
+
+    PipRegistryPath.Buffer = ExAllocatePoolWithTag(PagedPool, PipRegistryPath.MaximumLength, 'pasI');
+    if (!PipRegistryPath.Buffer)
+    {
+        DPRINT1("DriverEntry: STATUS_INSUFFICIENT_RESOURCES\n");
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+    RtlCopyMemory(PipRegistryPath.Buffer, RegistryPath->Buffer, RegistryPath->MaximumLength);
+
+    //KeInitializeEvent(&PipDeviceTreeLock, SynchronizationEvent, TRUE);
+    //KeInitializeEvent(&IsaBusNumberLock, SynchronizationEvent, TRUE);
+
+    BusNumBM = &BusNumBMHeader;
+
+    RtlInitializeBitMap(&BusNumBMHeader, BusNumberBuffer, 0x40);
+    RtlClearAllBits(BusNumBM);
+
+    //PipIsolationDisabled = PipIsIsolationDisabled();
+
+    return STATUS_SUCCESS;
+}
 /* EOF */
