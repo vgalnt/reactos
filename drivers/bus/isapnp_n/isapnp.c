b79bee90b9889277f24697882ed8a6ae3b6e970f
Vadim Galyant <vgal@rambler.ru>
2023-12-03 23:07:26 +0300

    [ISAPNP_N] Add "PNP ISA Bus Driver" (isapnp.sys).

---
+++ drivers/bus/isapnp_n/isapnp.c
@@ -0,0 +1,21 @@
+/*
+ * PROJECT:     ISA PnP Bus driver for NT 5.x
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Driver code
+ * COPYRIGHT:   Copyright 2019, 2023 Vadim Galyant <vgal@rambler.ru>
+ */
+
+/* INCLUDES *******************************************************************/
+
+#include <isapnp.h>
+
+//#define NDEBUG
+#include <debug.h>
+
+/* GLOBALS *******************************************************************/
+
+
+/* FUNCTIONS ******************************************************************/
+
+
+/* EOF */
---
+++ drivers/bus/isapnp_n/isapnp.h
@@ -0,0 +1,21 @@
+/*
+ * PROJECT:     ISA PnP Bus driver for NT 5.x
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Main header file
+ * COPYRIGHT:   Copyright 2019, 2023 Vadim Galyant <vgal@rambler.ru>
+ */
+
+#ifndef _ISAPNP_H_
+#define _ISAPNP_H_
+
+/* INCLUDES *******************************************************************/
+
+#include <wdm.h>
+
+/* STRUCTURES ***************************************************************/
+
+
+/* FUNCTIONS ****************************************************************/
+
+
+#endif /* _ISAPNP_H_ */
