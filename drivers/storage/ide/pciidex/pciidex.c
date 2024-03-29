0e111b2e8895b580bd1aa80f4c7e40fd21cb6d19
Vadim Galyant <vgal@rambler.ru>
2024-03-29 23:44:21 +0300

    [PCIIDEX] Implement PciIdePowerCompletionRoutine().

--- drivers/storage/ide/pciidex/pciidex.c
+++ drivers/storage/ide/pciidex/pciidex.c
@@ -918,8 +918,19 @@ PciIdePowerCompletionRoutine(
     _In_ PIRP Irp,
     _In_ PVOID Context)
 {
-    UNIMPLEMENTED_DBGBREAK();
-    return STATUS_NOT_IMPLEMENTED;
+    PIDE_WAIT_CONTEXT WaitContext = Context;
+
+    DPRINT("PciIdePowerCompletionRoutine: %p, %X, %X\n", DeviceObject, Irp, Context);
+
+    if (WaitContext)
+    {
+        WaitContext->Status = Irp->IoStatus.Status;
+        KeSetEvent(&WaitContext->Event, EVENT_INCREMENT, FALSE);
+    }
+
+    IoFreeIrp(Irp);
+
+    return STATUS_MORE_PROCESSING_REQUIRED;
 }
 
 NTSTATUS
