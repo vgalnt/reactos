47d07c61a82c3b2ab317fd07731cf16fb8811af5
Vadim Galyant <vgal@rambler.ru>
2024-04-10 22:22:43 +0400

    [ATAPI] Implement ChannelQueryPnPDeviceState().

--- drivers/storage/ide/atapi/atapi.c
+++ drivers/storage/ide/atapi/atapi.c
@@ -2132,11 +2132,20 @@ ChannelQueryId(
 NTSTATUS
 NTAPI
 ChannelQueryPnPDeviceState(
-    _In_ PDEVICE_OBJECT DeviceObject,
+    _In_ PDEVICE_OBJECT Fdo,
     _In_ PIRP Irp)
 {
-    UNIMPLEMENTED_DBGBREAK();
-    return STATUS_NOT_IMPLEMENTED;
+    PFDO_DEVICE_EXTENSION FdoExtension = Fdo->DeviceExtension;
+
+    DPRINT("ChannelQueryPnPDeviceState: QUERY_DEVICE_STATE for FDOE %p\n", FdoExtension);
+
+    if (FdoExtension->Paging)
+        Irp->IoStatus.Information |= 0x20;
+
+    Irp->IoStatus.Status = STATUS_SUCCESS;
+    IoSkipCurrentIrpStackLocation(Irp);
+
+    return IoCallDriver(FdoExtension->LowDevice, Irp);
 }
 
 NTSTATUS
