da48eda97127e50e7bbab7ca1e77d6918aaf7dd6
Vadim Galyant <vgal@rambler.ru>
2024-05-30 12:58:19 +0400

    [ATAPI] Implement IdePortNoSupportIrp().

--- drivers/storage/ide/atapi/atapi.c
+++ drivers/storage/ide/atapi/atapi.c
@@ -3521,8 +3521,19 @@ IdePortNoSupportIrp(
     _In_ PDEVICE_OBJECT Pdo,
     _In_ PIRP Irp)
 {
-    UNIMPLEMENTED_DBGBREAK();
-    return STATUS_NOT_IMPLEMENTED;
+    PIO_STACK_LOCATION IoStack;
+    NTSTATUS Status;
+
+    IoStack = IoGetCurrentIrpStackLocation(Irp);
+    Status = Irp->IoStatus.Status;
+
+    if (IoStack->MajorFunction == IRP_MJ_POWER)
+        PoStartNextPowerIrp(Irp);
+
+    DPRINT("IdePortNoSupportIrp: %p failing unsupported Irp (%X:%X) with %X\n", Pdo, IoStack->MajorFunction, IoStack->MinorFunction, Status);
+
+    IoCompleteRequest(Irp, 0);
+    return Status;
 }
 
 NTSTATUS
