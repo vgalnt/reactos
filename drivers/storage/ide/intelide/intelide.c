40f0b4db00bfebf4abdd4315738dc4e24ac252c6
Vadim Galyant <vgal@rambler.ru>
2024-07-12 14:43:57 +0400

    [INTELIDE] Implement PiixIdeTransferModeSelect().

--- drivers/storage/ide/intelide/intelide.c
+++ drivers/storage/ide/intelide/intelide.c
@@ -106,12 +106,180 @@ PiixIdeUdmaModesSupported(
 
 NTSTATUS
 NTAPI
+PiixIdepTransferModeSelect(
+    _In_ PINTEL_CONTROLLER_EXTENSION DeviceExtension,
+    _In_ PPCIIDE_TRANSFER_MODE_SELECT XferModeSelect,
+    _Out_ ULONG* OutXferMode,
+    _Out_ INTEL_MODES_TIMING_AND_CONTROL* OutTimingAndControl,
+    _Out_ INTEL_SLAVE_IDE_TIMING* OutSlaveTiming,
+    _Out_ INTEL_ULTRA_DMA_CONTROL* OutUdmaControl,
+    _Out_ INTEL_ULTRA_DMA_TIMING* OutUdmaTiming)
+{
+    UNIMPLEMENTED_DBGBREAK();
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
 PiixIdeTransferModeSelect(
     _In_ PVOID InDeviceExtension,
     _In_ PPCIIDE_TRANSFER_MODE_SELECT XferModeSelect)
 {
-    UNIMPLEMENTED_DBGBREAK();
-    return STATUS_NOT_IMPLEMENTED;
+    PINTEL_CONTROLLER_EXTENSION DeviceExtension = InDeviceExtension;
+    INTEL_MODES_TIMING_AND_CONTROL TimingAndControl;
+    INTEL_MODES_TIMING_AND_CONTROL OldTimingAndControl[2];
+    INTEL_SLAVE_IDE_TIMING SlaveTiming;
+    INTEL_SLAVE_IDE_TIMING OldSlaveTiming;
+    INTEL_ULTRA_DMA_CONTROL UdmaControl;
+    INTEL_ULTRA_DMA_CONTROL OldUdmaControl;
+    INTEL_ULTRA_DMA_TIMING UdmaTiming;
+    INTEL_ULTRA_DMA_TIMING OldUdmaTiming;
+    ULONG XferMode[2];
+    ULONG Channel;
+    ULONG ix;
+    USHORT DeviceId;
+    USHORT DataMask;
+    NTSTATUS Status;
+
+    DPRINT("PiixIdeTransferModeSelect: DeviceExtension %p\n", DeviceExtension);
+
+    for (ix = 0; ix < 2; ix++)
+    {
+        RtlCopyMemory(&DeviceExtension->IdentifyData[ix],
+                      &XferModeSelect->IdentifyData[ix],
+                      sizeof(DeviceExtension->IdentifyData[ix]));
+    }
+
+    Status = PiixIdepTransferModeSelect(DeviceExtension,
+                                        XferModeSelect,
+                                        XferMode,
+                                        &TimingAndControl,
+                                        &SlaveTiming,
+                                        &UdmaControl,
+                                        &UdmaTiming);
+    if (!NT_SUCCESS(Status))
+    {
+        DPRINT1("PiixIdeTransferModeSelect: Status %X\n", Status);
+        goto Finish;
+    }
+
+    OldSlaveTiming.AsUCHAR = 0;
+    OldUdmaControl.AsUCHAR = 0;
+    OldUdmaTiming.AsUSHORT = 0;
+
+    Channel = XferModeSelect->Channel;
+
+    /* 40–41 IDE TIMING (Primary) R/W, 42–43 IDE TIMING (Secondary) R/W */
+    PciIdeXGetBusData(DeviceExtension, OldTimingAndControl, 0x40, 4);
+
+    DPRINT("PiixIdeTransferModeSelect: Old IDETIM %X\n", OldTimingAndControl[Channel].AsUSHORT);
+
+    DeviceId = DeviceExtension->DeviceId;
+    if (DeviceId != 0x1230)
+    {
+        /* 44 Slave IDE Timing (Primary and Secondary) R/W */
+        PciIdeXGetBusData(DeviceExtension, &OldSlaveTiming, 0x44, 1);
+        DPRINT("PiixIdeTransferModeSelect: Old SIDETIM %X\n", OldSlaveTiming.AsUCHAR);
+    }
+
+    if (DeviceId == 0x7111 || DeviceId == 0x2421 || DeviceId == 0x7601 || DeviceId == 0x2411 || DeviceId == 0x7199 ||
+        DeviceId == 0x2441 || DeviceId == 0x244A || DeviceId == 0x244B || DeviceId == 0x248A || DeviceId == 0x248B ||
+        DeviceId == 0x24C1 || DeviceId == 0x24CA || DeviceId == 0x24CB || DeviceId == 0x24D1 || DeviceId == 0x24DB ||
+        DeviceId == 0x25A2 || DeviceId == 0x25A3 || DeviceId == 0x2651 || DeviceId == 0x2652 || DeviceId == 0x2653 ||
+        DeviceId == 0x266F)
+    {
+        /* 48 Ultra DMA Control Register R/W */
+        PciIdeXGetBusData(DeviceExtension, &OldUdmaControl, 0x48, 1);
+        DPRINT("PiixIdeTransferModeSelect: Old SDMACTL %X\n", OldUdmaControl.AsUCHAR);
+
+        /* 4A:4B Ultra DMA Timing Register R/W */
+        PciIdeXGetBusData(DeviceExtension, &OldUdmaTiming, (0x4A + Channel), 2);
+        DPRINT("PiixIdeTransferModeSelect: Old SDMATIM %X\n", OldUdmaTiming.AsUSHORT);
+    }
+
+    if (DeviceId == 0x2411 || DeviceId == 0x2421 || DeviceId == 0x7601)
+    {
+        UNIMPLEMENTED_DBGBREAK();
+    }
+
+    if (Channel == 0)
+    {
+        OldSlaveTiming.AsUCHAR = ((OldSlaveTiming.AsUCHAR & ~0xF) | (SlaveTiming.AsUCHAR & 0xF));
+        OldUdmaControl.AsUCHAR = ((OldUdmaControl.AsUCHAR & ~3) | (UdmaControl.AsUCHAR & 3));
+    }
+    else
+    {
+        OldSlaveTiming.AsUCHAR = ((OldSlaveTiming.AsUCHAR & ~0xF0) | (SlaveTiming.AsUCHAR & 0xF0));
+        OldUdmaControl.AsUCHAR = ((OldUdmaControl.AsUCHAR & ~0xC) | (UdmaControl.AsUCHAR & 0xC));
+    }
+
+    DPRINT("PiixIdeTransferModeSelect: New PIIX/ICH values - IDETIM %X, SIDETIM %X, SDMACTL %X, SDMATIM %X\n", 
+           TimingAndControl.AsUSHORT, OldSlaveTiming.AsUCHAR, OldUdmaControl.AsUCHAR, UdmaTiming.AsUSHORT);
+
+    DataMask = 0xFFFF;
+
+    Status = PciIdeXSetBusData(DeviceExtension, &TimingAndControl, &DataMask, (0x40 + Channel * 2), 2);
+    if (!NT_SUCCESS(Status))
+    {
+        DPRINT1("PiixIdeTransferModeSelect: Status %X\n", Status);
+        goto Finish;
+    }
+
+    if (DeviceId != 0x1230)
+    {
+        DataMask = (!XferModeSelect->Channel ? 0xF : 0xF0);
+
+        Status = PciIdeXSetBusData(DeviceExtension, &SlaveTiming, &DataMask, 0x44, 1);
+        if (!NT_SUCCESS(Status))
+        {
+            DPRINT1("PiixIdeTransferModeSelect: Status %X\n", Status);
+            ASSERT(!"Unable to set pci config data\n");
+            goto Finish;
+        }
+    }
+
+    if (DeviceId == 0x7111 || DeviceId == 0x2421 || DeviceId == 0x7601 || DeviceId == 0x2411 || DeviceId == 0x7199 ||
+        DeviceId == 0x2441 || DeviceId == 0x244A || DeviceId == 0x244B || DeviceId == 0x248A || DeviceId == 0x248B ||
+        DeviceId == 0x24C1 || DeviceId == 0x24CA || DeviceId == 0x24CB || DeviceId == 0x24D1 || DeviceId == 0x24DB ||
+        DeviceId == 0x25A2 || DeviceId == 0x25A3 || DeviceId == 0x2651 || DeviceId == 0x2652 || DeviceId == 0x2653 ||
+        DeviceId == 0x266F)
+    {
+        DataMask = (!XferModeSelect->Channel ? 3 : 0xC);
+
+        Status = PciIdeXSetBusData(DeviceExtension, &UdmaControl, &DataMask, 0x48, 1);
+        if (!NT_SUCCESS(Status))
+        {
+            DPRINT1("PiixIdeTransferModeSelect: Status %X\n", Status);
+            ASSERT(!"Unable to set pci config data\n");
+            goto Finish;
+        }
+
+        DataMask = 0xFF;
+
+        Status = PciIdeXSetBusData(DeviceExtension, &UdmaTiming, &DataMask, (0x4A + Channel), 2);
+        if (!NT_SUCCESS(Status))
+        {
+            DPRINT1("PiixIdeTransferModeSelect: Status %X\n", Status);
+            ASSERT(!"Unable to set pci config data\n");
+            goto Finish;
+        }
+    }
+
+    if (DeviceId == 0x2411 || DeviceId == 0x2421 || DeviceId == 0x7601 ||
+        DeviceId == 0x2441 || DeviceId == 0x244A || DeviceId == 0x244B)
+    {
+        UNIMPLEMENTED_DBGBREAK();
+    }
+
+Finish:
+
+    if (NT_SUCCESS(Status))
+    {
+        for (ix = 0; ix < 2; ix++)
+            XferModeSelect->DeviceTransferModeSelected[ix] = XferMode[ix];
+    }
+
+    return Status;
 }
 
 NTSTATUS
--- drivers/storage/ide/intelide/intelide.h
+++ drivers/storage/ide/intelide/intelide.h
@@ -37,6 +37,59 @@ typedef struct _INTEL_MODES_TIMING_AND_CONTROL
     };
 } INTEL_MODES_TIMING_AND_CONTROL, *PINTEL_MODES_TIMING_AND_CONTROL;
 
+typedef struct _INTEL_SLAVE_IDE_TIMING
+{
+    union
+    {
+        struct
+        {
+            UCHAR RecoveryTime1:2;
+            UCHAR IordySamplePoint1:2;
+            UCHAR RecoveryTime2:2;
+            UCHAR IordySamplePoint2:2;
+        };
+
+        UCHAR AsUCHAR;                        
+    };
+} INTEL_SLAVE_IDE_TIMING, *PINTEL_SLAVE_IDE_TIMING;
+
+typedef struct _INTEL_ULTRA_DMA_CONTROL
+{
+    union
+    {
+        struct
+        {
+            UCHAR PSDE0:1; // Primary Drive 0 Ultra DMA Mode Enable
+            UCHAR PSDE1:1; // Primary Drive 1 Ultra DMA Mode Enable
+            UCHAR SSDE0:1; // Secondary Drive 0 Ultra DMA Mode Enable
+            UCHAR SSDE1:1; // Secondary Drive 1 Ultra DMA Mode Enable
+            UCHAR Reserved:4; // 0000h
+        };
+
+        UCHAR AsUCHAR;                        
+    };
+} INTEL_ULTRA_DMA_CONTROL, *PINTEL_ULTRA_DMA_CONTROL;
+
+typedef struct _INTEL_ULTRA_DMA_TIMING
+{
+    union
+    {
+        struct
+        {
+            USHORT PCT0:2; // Primary Drive 0 Ultra DMA Cycle Time
+            USHORT Reserved1:2; // 00h
+            USHORT PCT1:2; // Primary Drive 1 Ultra DMA Cycle Time
+            USHORT Reserved2:2; // 00h
+            USHORT SCT0:2; // Secondary Drive 0 Ultra DMA Cycle Time
+            USHORT Reserved3:2; // 00h
+            USHORT SCT1:2; //  Secondary Drive 1 Ultra DMA Cycle Time
+            USHORT Reserved4:2; // 00h
+        };
+
+        USHORT AsUSHORT;                        
+    };
+} INTEL_ULTRA_DMA_TIMING, *PINTEL_ULTRA_DMA_TIMING;
+
 #include <poppack.h>
 
 typedef struct _INTEL_CONTROLLER_EXTENSION
@@ -45,4 +98,5 @@ typedef struct _INTEL_CONTROLLER_EXTENSION
     UCHAR Pad[2];
     ULONG SupportedTransferMode[2][2];
     ULONG UdmaSpeed;
+    IDENTIFY_DATA IdentifyData[2];
 } INTEL_CONTROLLER_EXTENSION, *PINTEL_CONTROLLER_EXTENSION;
