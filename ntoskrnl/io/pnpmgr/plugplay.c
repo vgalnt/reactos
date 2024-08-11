71c7fc45bbea1156c4cc7f1ab8108d0f6b17b695
Vadim Galyant <vgal@rambler.ru>
2024-08-12 00:55:26 +0400

    [NTOS:PNP] Use PPLUGPLAY_CONTROL_DEVICE_CONTROL_DATA instead PPLUGPLAY_CONTROL_RESET_DEVICE_DATA.

--- ntoskrnl/io/pnpmgr/plugplay.c
+++ ntoskrnl/io/pnpmgr/plugplay.c
@@ -334,8 +334,8 @@ NTSTATUS NTAPI PiControlInitializeDevice(ULONG PnPControlClass, PVOID PnPControl
 NTSTATUS NTAPI PiControlStartDevice(ULONG PnPControlClass, PVOID PnPControlData, ULONG PnPControlDataLength, KPROCESSOR_MODE AccessMode)
 {
     UNIMPLEMENTED;
-    ASSERT(FALSE); // IoDbgBreakPointEx();
-    return STATUS_NOT_IMPLEMENTED;
+    //ASSERT(FALSE); // IoDbgBreakPointEx();
+    return 0;//STATUS_NOT_IMPLEMENTED;
 }
 
 NTSTATUS NTAPI PiControlQueryAndRemoveDevice(ULONG PnPControlClass, PVOID PnPControlData, ULONG PnPControlDataLength, KPROCESSOR_MODE AccessMode)
@@ -927,7 +927,7 @@ PiQueueDeviceRequest(
 
 NTSTATUS NTAPI PiControlResetDevice(ULONG PnPControlClass, PVOID PnPControlData, ULONG PnPControlDataLength, KPROCESSOR_MODE AccessMode)
 {
-    PPLUGPLAY_CONTROL_RESET_DEVICE_DATA ResetData = PnPControlData;
+    PPLUGPLAY_CONTROL_DEVICE_CONTROL_DATA ResetData = PnPControlData;
     USHORT Size = ResetData->DeviceInstance.Length;
     UNICODE_STRING DeviceInstance;
     NTSTATUS Status;
