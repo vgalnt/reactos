93ea8e20128c07cb09e63962d9a347c8c75b17ed
Vadim Galyant <vgal@rambler.ru>
2024-08-19 01:56:34 +0400

    [MOUNTMGR] Fix MountMgrTargetDeviceNotification().

--- drivers/storage/mountmgr/notify.c
+++ drivers/storage/mountmgr/notify.c
@@ -237,8 +237,11 @@ MountMgrTargetDeviceNotification(IN PVOID NotificationStructure,
     DeviceExtension = DeviceInformation->DeviceExtension;
     Notification = NotificationStructure;
 
-    /* The notification have to be unregistered already (in device interface change handler) */
-    ASSERT(!IsEqualGUID(&Notification->Event, &GUID_TARGET_DEVICE_REMOVE_COMPLETE));
+    if (IsEqualGUID(&Notification->Event, &GUID_TARGET_DEVICE_REMOVE_COMPLETE))
+    {
+        MountMgrMountedDeviceRemoval(DeviceExtension, &DeviceInformation->SymbolicName);
+        return STATUS_SUCCESS;
+    }
 
     /* It it's to signal that a volume has been mounted
      * Verify if a database sync is required and execute it
