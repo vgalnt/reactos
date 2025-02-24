071faf23c6a63e36081b15b99844c99066c67dbb
Vadim Galyant <vgal@rambler.ru>
2025-02-24 20:35:09 +0300

    [KDNET] Add struct _KDNET_EXTENSIBILITY_EXPORT.

--- drivers/base/kdnet/kdnet.h
+++ drivers/base/kdnet/kdnet.h
@@ -41,6 +41,181 @@ typedef struct _KD_NIC_DATA
 } KD_NIC_DATA, *PKD_NIC_DATA;
 C_ASSERT(sizeof(KD_NIC_DATA) == 0x40);
 
+typedef
+ULONG
+(NTAPI* KDNET_GET_PCI_DATA_BY_OFFSET)(
+    ULONG BusNumber,
+    ULONG SlotNumber,
+    PVOID Buffer,
+    ULONG Offset,
+    ULONG Length
+);
+
+typedef
+ULONG
+(NTAPI* KDNET_SET_PCI_DATA_BY_OFFSET)(
+    ULONG BusNumber,
+    ULONG SlotNumber,
+    PVOID Buffer,
+    ULONG Offset,
+    ULONG Length
+);
+
+typedef
+PHYSICAL_ADDRESS
+(NTAPI* KDNET_GET_PHYSICAL_ADDRESS)(
+    PVOID Va
+);
+
+typedef
+void
+(NTAPI* KDNET_STALL_EXECUTION_PROCESSOR)(
+    ULONG Microseconds
+);
+
+typedef
+UCHAR
+(NTAPI* KDNET_READ_REGISTER_UCHAR)(
+    PUCHAR Register
+);
+
+typedef
+USHORT
+(NTAPI* KDNET_READ_REGISTER_USHORT)(
+    PUSHORT Register
+);
+
+typedef
+ULONG
+(NTAPI* KDNET_READ_REGISTER_ULONG)(
+    PULONG Register
+);
+
+typedef
+ULONG64
+(NTAPI* KDNET_READ_REGISTER_ULONG64)(
+    PULONG64 Register
+);
+
+typedef
+void
+(NTAPI* KDNET_WRITE_REGISTER_UCHAR)(
+    PUCHAR Register,
+    UCHAR Value
+);
+
+typedef
+void
+(NTAPI* KDNET_WRITE_REGISTER_USHORT)(
+    PUSHORT Register,
+    USHORT Value
+);
+
+typedef
+void
+(NTAPI* KDNET_WRITE_REGISTER_ULONG)(
+    PULONG Register,
+    ULONG Value
+);
+
+typedef
+void
+(NTAPI* KDNET_WRITE_REGISTER_ULONG64)(
+    PULONG64 Register,
+    ULONG64 Value
+);
+
+typedef
+UCHAR
+(NTAPI* KDNET_READ_PORT_UCHAR)(
+    PUCHAR Port
+);
+
+typedef
+USHORT
+(NTAPI* KDNET_READ_PORT_USHORT)(
+    PUSHORT Port
+);
+
+typedef
+ULONG
+(NTAPI* KDNET_READ_PORT_ULONG)(
+    PULONG Port
+);
+
+typedef
+ULONG
+(NTAPI* KDNET_READ_PORT_ULONG64)(
+    PULONG64 Port
+);
+
+typedef
+void
+(NTAPI* KDNET_WRITE_PORT_UCHAR)(
+    PUCHAR Port,
+    UCHAR Value
+);
+
+typedef
+void
+(NTAPI* KDNET_WRITE_PORT_USHORT)(
+    PUSHORT Port,
+    USHORT Value
+);
+
+typedef
+void
+(NTAPI* KDNET_WRITE_PORT_ULONG)(
+    PULONG Port,
+    ULONG Value
+);
+
+typedef
+void
+(NTAPI* KDNET_WRITE_PORT_ULONG64)(
+    PULONG Port,
+    ULONG64 Value
+);
+
+typedef
+void
+(NTAPI* KDNET_SET_HIBER_RANGE)(
+    PVOID MemoryMap,
+    ULONG Flags,
+    PVOID Address,
+    ULONG_PTR Length,
+    ULONG Tag
+);
+
+typedef struct _KDNET_EXTENSIBILITY_EXPORT
+{
+    ULONG FunctionCount;
+    KDNET_GET_PCI_DATA_BY_OFFSET GetPciDataByOffset;
+    KDNET_SET_PCI_DATA_BY_OFFSET SetPciDataByOffset;
+    KDNET_GET_PHYSICAL_ADDRESS GetPhysicalAddress;
+    KDNET_STALL_EXECUTION_PROCESSOR StallExecutionProcessor;
+    KDNET_READ_REGISTER_UCHAR ReadRegisterUChar;
+    KDNET_READ_REGISTER_USHORT ReadRegisterUShort;
+    KDNET_READ_REGISTER_ULONG ReadRegisterULong;
+    KDNET_READ_REGISTER_ULONG64 ReadRegisterULong64;
+    KDNET_WRITE_REGISTER_UCHAR WriteRegisterUChar;
+    KDNET_WRITE_REGISTER_USHORT WriteRegisterUShort;
+    KDNET_WRITE_REGISTER_ULONG WriteRegisterULong;
+    KDNET_WRITE_REGISTER_ULONG64 WriteRegisterULong64;
+    KDNET_READ_PORT_UCHAR ReadPortUChar;
+    KDNET_READ_PORT_USHORT ReadPortUShort;
+    KDNET_READ_PORT_ULONG ReadPortULong;
+    KDNET_READ_PORT_ULONG64 ReadPortULong64;
+    KDNET_WRITE_PORT_UCHAR WritePortUChar;
+    KDNET_WRITE_PORT_USHORT WritePortUShort;
+    KDNET_WRITE_PORT_ULONG WritePortULong;
+    KDNET_WRITE_PORT_ULONG64 WritePortULong64;
+    NTSTATUS* _KdNetErrorStatus;
+    PWCHAR* _KdNetErrorString;
+    ULONG* _KdNetHardwareID;
+    KDNET_SET_HIBER_RANGE SetHiberRange;
+} KDNET_EXTENSIBILITY_EXPORT, *PKDNET_EXTENSIBILITY_EXPORT;
+
 
 #endif /* _KDNET_H_ */
 
