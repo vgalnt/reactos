b679dc18d8658dccd4f539c7c37a33f22f14551b
Vadim Galyant <vgal@rambler.ru>
2025-02-24 20:21:49 +0300

    [KDNET] Add struct _KD_NIC_DATA.

--- drivers/base/kdnet/kdnet.h
+++ drivers/base/kdnet/kdnet.h
@@ -21,6 +21,26 @@ typedef enum
     KDP_PACKET_RESEND = 2
 } KDP_STATUS;
 
+typedef struct _KD_NIC_DATA
+{
+    USHORT Version;
+    USHORT Size;
+    ULONG Reserved0;
+    NTSTATUS Status;
+    ULONG LinkSpeed1;
+    ULONG Reserved1;
+    ULONG Reserved2;
+    SLIST_HEADER sListHead;
+    SLIST_HEADER sListHead1;
+    SLIST_HEADER sListHead2;
+    UCHAR MacAddress[6];
+    UCHAR LinkState;
+    UCHAR Reserved3;
+    ULONG LinkSpeed2;
+    ULONG Reserved4;
+} KD_NIC_DATA, *PKD_NIC_DATA;
+C_ASSERT(sizeof(KD_NIC_DATA) == 0x40);
+
 
 #endif /* _KDNET_H_ */
 
